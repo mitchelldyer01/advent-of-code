e5ca86409e499fecbe03846c02d9269f08d3d5bf
mitchelldyer01@gmail.com <mitchelldyer01@gmail.com>
2023-12-04 19:00:08 -0700

    day 4 part 2

--- 04/src/main.rs
+++ 04/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 use std::io::{self, BufRead};
 use std::error::Error;
 
@@ -8,14 +8,30 @@ fn main() -> Result<(), Box<dyn Error>> {
     let file = File::open(file_path)?;
     let reader = io::BufReader::new(file);
 
+    let mut copies: HashMap<usize, usize> = HashMap::new();
+
     let mut sum = 0;
+    let mut sum_of_cards = 0;
 
     for line in reader.lines() {
         // trim up to :
         let games = line?;
         let results = games.split(':').last().unwrap();
+        let current_game = games.split(':').next().unwrap().split(' ').last().unwrap().parse::<usize>().unwrap();
+
+        let copies_of_current_game: usize;
+        match copies.get(&current_game) {
+            Some(amount) => {
+                copies_of_current_game = *amount;
+            },
+            None => {
+                copies_of_current_game = 0;
+            }
+        }
 
         let numbers: Vec<&str> = results.split('|').map(|s| s.trim()).collect();
+
+        // 1.
         if let [winners, picks] = numbers.as_slice() {
             let set_winners: HashSet<u32> = winners
                 .split_whitespace()
@@ -32,10 +48,32 @@ fn main() -> Result<(), Box<dyn Error>> {
             if intersection.len() > 0 {
                 let local_sum = usize::pow(2, (intersection.len()-1).try_into().unwrap());
                 sum += local_sum;
+
+                // 2.
+                for i in 0..intersection.len() {
+                    let scratchcard = current_game+i+1;
+                    
+                    match copies.get(&scratchcard) {
+                        Some(amount) => {
+                            copies.insert(scratchcard, 1 + amount + copies_of_current_game);
+                        },
+                        None => {
+                            copies.insert(scratchcard, 1 + copies_of_current_game);
+                        },
+
+                    }
+                }
             }
+
+            println!("game {}: copies: {}", current_game, copies_of_current_game);
+
+            sum_of_cards += 1 + copies_of_current_game;
         }
+
+
     }
 
     println!("Sum of winning picks: {}", sum);
+    println!("Sum of total scratchcards and copies: {}", sum_of_cards);
     Ok(())
 }
